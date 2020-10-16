275ab5534084bf9f10379d148d2ad4666b87be57
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-10-16 17:21:21 -0500

    Subiendo prueba con servlet

---
+++ Ejemplos/BaseProject/src/main/java/com/escom/wad/baseproject/controller/EventoServlet.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.escom.wad.baseproject.controller;
+
+import com.escom.wad.baseproject.dao.EventoDAO;
+import com.escom.wad.baseproject.model.Evento;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author aarongarcia
+ */
+public class EventoServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EventoServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EventoServlet at " + request.getContextPath() + "</h1>");
+            
+            
+            Evento evento = new Evento();
+            evento.setIdEvento(3);
+            evento.setNombreEvento(request.getParameter("txtnombre"));
+            evento.setSede(request.getParameter("txtsede"));
+            evento.setFechaInicio(Date.valueOf("2020-10-19"));
+            evento.setFechaFin(Date.valueOf("2020-10-23"));
+            
+            EventoDAO dao = new EventoDAO();
+            try {
+                dao.create(evento);
+            } catch (SQLException ex) {
+                Logger.getLogger(EventoServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+                    
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
